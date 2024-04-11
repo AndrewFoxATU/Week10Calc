3b89c20eec81dd3ab56fb2d0a1ec5d356aa30dce
AndrewFoxATU <G00417373@atu.ie>
2024-04-11 12:13:31 +0100

    finished

--- src/main/java/ie/atu/week10/Calc.java
+++ src/main/java/ie/atu/week10/Calc.java
@@ -5,4 +5,18 @@ public class Calc {
         int result = firstNum + secondNum;
         return result;
     }
+    public int sub(int firstNum, int secondNum) {
+        int result2 = firstNum - secondNum;
+        return result2;
+    }
+
+    public int div(int firstNum, int secondNum) {
+        int result3 = firstNum / secondNum;
+        return result3;
+    }
+
+    public int mul(int firstNum, int secondNum) {
+        int result4 = firstNum * secondNum;
+        return result4;
+    }
 }
--- src/test/java/ie/atu/week10/calcTest.java
+++ src/test/java/ie/atu/week10/calcTest.java
@@ -6,12 +6,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class calcTest {
     Calc newCalc;
-
-
+    Calc newCalc2;
+    Calc newCalc3;
+    Calc newCalc4;
     @Test
     void testAdd(){
         newCalc = new Calc();
         assertEquals(10,newCalc.add(4,6));
+
+    }
+    @Test
+    void testSub(){
+        newCalc2 = new Calc();
+        assertEquals(2,newCalc2.sub(6,4));
+
+    }
+
+    @Test
+    void testDiv(){
+        newCalc3 = new Calc();
+        assertEquals(2,newCalc3.div(8,4));
+
+    }
+
+    @Test
+    void testMul(){
+        newCalc4 = new Calc();
+        assertEquals(16,newCalc4.mul(8,2));
 
     }
 
